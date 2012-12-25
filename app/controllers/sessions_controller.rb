515ddbb7e677efa5259be0a9a54247995c6091c6
ash <ashskutches@gmail.com>
2012-12-24 21:48:13 -0800

    Ability to log out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,4 +4,9 @@ class SessionsController < ApplicationController
     session[:user_id] = user.id
     redirect_to root_url
   end
+
+  def destroy
+   reset_session 
+   redirect_to root_url
+  end
 end

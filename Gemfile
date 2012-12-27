300419fd7c7404660cd306e3490b782d1fe92443
ash <ashskutches@gmail.com>
2012-12-27 01:59:38 -0800

    Add ability to look through facebook friends

--- Gemfile
+++ Gemfile
@@ -7,7 +7,7 @@ gem 'rails', '3.2.9'
 
 gem 'sqlite3'
 gem 'omniauth-facebook', '1.4.0'
-
+gem 'koala'
 
 # Gems used only for assets and not required
 # in production environments by default.
--- app/models/user.rb
+++ app/models/user.rb
@@ -13,11 +13,33 @@ class User < ActiveRecord::Base
       user.oauth_token = auth.credentials.token
       user.oauth_expires_at = Time.at(auth.credentials.expires_at)
       user.save!
+      user.talk("Your favorite movie is #{user.favorite_television_shows}")
 
     end
   end
 
-  def talk
-    system "espeak 'Hi #{name}, my name is Nigel."
+  def talk(content)
+    system "espeak 'Your favorite television shows are #{content}'"
   end
+
+  def facebook
+    @facebook ||= Koala::Facebook::API.new(oauth_token)
+    block_given? ? yield(@facebook) : @facebook
+  rescue Koala::Facebook::APIError
+    logger.info e.to_s
+    nil
+  end
+
+  def favorite_television_shows
+    facebook.get_connection("me", "television").collect { |show| show['name']}
+  end
+
+  def facebook_friends
+    facebook.get_connection("me", "friends").collect { |friend| friend['name']}
+  end
+
+  def is_facebook_friend?(name)
+    !(facebook_friends.grep(name).empty?)
+  end
+
 end

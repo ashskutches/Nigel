d642c52cdc6a9e3d470273acce352bb69376949a
ash <ash@nigel.(none)>
2013-05-16 00:10:12 -0700

    Add feature: Facebook messages spoken

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,12 @@
 class DashboardController < ApplicationController
 
   def homepage
+    if current_user  
+     Nigal.gather_knowledge(current_user)
+    end
     current_user ? @tasks = current_user.tasks.all : nil
+    @tasks.each { |task| Nigal.speak(task.content) }
+    Task.destroy_all
   end
 
 end
--- app/models/nigal.rb
+++ app/models/nigal.rb
@@ -1,9 +1,17 @@
 class Nigal < ActiveRecord::Base
   # attr_accessible :title, :body
 
-  def self.talk(content)
+  def self.speak(content)
     system "espeak '#{content}'"
   end
 
-
+  def self.gather_knowledge(user)
+    if user.facebook_new_messages? 
+      messages = user.facebook_new_messages
+      messages.each do |message| 
+        task = user.tasks.build(:content => "Facebook message from #{message[:name]}. #{message[:message]}", :source => "Facebook", :user_id => user.id, :action => "delete")
+        task.save
+      end
+    end
+  end
 end

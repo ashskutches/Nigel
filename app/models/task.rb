02c18a734d06831b9f908c5ea271c2f22ede853c
ash <ash@ubuntu.(none)>
2013-06-15 14:58:17 -0700

    Refactor update tasks

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,20 +4,27 @@ class Task < ActiveRecord::Base
   def self.update_facebook_content(users)
     users.each do |user|
       messages = user.facebook_new_messages? ? user.facebook_new_messages : nil
-      if messages
-        messages.each do |message| 
-          task = user.tasks.build(:content => "Facebook message from #{message[:name]}. #{message[:message]}", :source => "Facebook", :user_id => user.id, :action => "new", :uid => "#{message[:id]}")
-	  puts message
-          task.save unless Task.all.map(&:uid).include?(task.uid) 
-        end
-      end
+      Task.build_from_facebook_messages(user, messages)
     end
   end
 
-  def self.mark_as_viewed
+  def self.build_from_facebook_messages(user, messages)
+    messages.each do |message|
+      task = user.tasks.build(
+        :content => " message from #{message[:name]}. #{message[:message]}", 
+        :source => "Facebook", 
+        :user_id => user.id, 
+        :action => "new", 
+        :uid => "#{message[:id]}")
+      puts message
+      task.save unless Task.all.map(&:uid).include?(task.uid) 
+    end
+  end
+
+  def mark_as_viewed
     action = 'viewed' if action == 'new'
     save
   end
-
+  
 end
 

d5ddb9d5ae7f6434a408a44dd4c8f1d85c7428d4
ash <ash@ubuntu.(none)>
2013-06-15 12:44:07 -0700

    Update_facebook_content takes an array now

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,7 @@
 class DashboardController < ApplicationController
 
   def homepage
-    Task.update_facebook_content(current_user) if current_user
+    Task.update_facebook_content([current_user]) if current_user
     @tasks = Task.all
   end
 

077edeb13ae70e065c9733cb09bb325de4b0590a
Ash Skutches <ashskutches@gmail.com>
2013-07-13 10:55:54 -0700

    Nil case

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,7 +2,7 @@ class DashboardController < ApplicationController
 
   def homepage
     @tasks = current_user ? current_user.tasks : nil
-    Task.mark_as_viewed(@tasks)
+    Task.mark_as_viewed(@tasks) if current_user
   end
 
 end

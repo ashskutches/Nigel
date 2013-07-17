be1abd419b7fb4437c7b0da7ae833f9b7c42b10f
Bob <ash@ubuntu.(none)>
2013-07-17 15:47:24 -0700

    Fix Facebook_Update script

--- lib/clock.rb
+++ lib/clock.rb
@@ -7,4 +7,6 @@ handler do |job|
   puts "Running #{job}"
 end
 
-every(1.minutes, Task.update_all_facebook_content)
+every(1.minutes, "Update Facebook content") do
+  Task.update_all_facebook_content
+end

8f278fee3eb30bb9eee286d77062f1464af332f5
Bob <ash@ubuntu.(none)>
2013-07-19 19:17:59 -0700

    Frontend: Add new messages

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -22,3 +22,51 @@ window.fbAsyncInit = function() {
     return true;
   });
 };
+
+
+$(document).ready(function() {
+
+  var text = ""
+
+  $.each($('li.task'), function() { 
+    if ($(this).data('action')) {
+      text = text + $(this).text();
+    }
+  });
+
+
+  window.setInterval(function(){
+    addNewTasks();
+  }, 5000);
+  
+  function addNewTasks() {
+    $.ajax({
+      url: "/user/1/tasks.json",
+      type: "get",
+      dataType: "json",
+      success: function(data) {
+        $.each(data, function() {
+          data = { action: 'viewed', id: this.id }
+          $('ul#tasks').prepend("<li class='task' data-action='" + this.action + "'> <p class='source'>" + this.source + "</p> <p class='content'>" + this.content + "</li>");
+	  updateTask(data);
+	  speak(this.source + this.content);
+        });
+      }
+    });
+  };
+
+  function updateTask(data) {
+    $.ajax({
+      type: 'PUT',
+      url:  '/user/1/tasks/' + data['id'] + '.json',
+      data: data,
+      dataType: "JSON",
+      success: function(data) {
+        console.log("Updated");
+      }
+    });
+  }
+
+
+//Document.ready  
+});
